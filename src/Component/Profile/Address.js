0581cf55676eafe5c9df1e548c466dad17c0aa0f
Kaisnamsi <kaisnamsi@gmail.com>
2023-03-15 19:17:38 +0100

    first commit

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Address() {
+  return (
+    <p>Mouala Gafsa</p>
+  );
+}
+
+export default Address;
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProfilePhoto() {
+  return (
+    <img
+      src="https://scontent.ftun10-1.fna.fbcdn.net/v/t39.30808-6/277674202_1940133582840409_1545159904072714066_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=RkcKdhuYRt4AX9mCqHk&_nc_ht=scontent.ftun10-1.fna&oh=00_AfCZDMjmbNZWorcwxwwKsbV7Lt_PeAXYCzj7U15DM0IcKw&oe=6417D21C"
+      alt="Profile"
+    />
+  );
+}
+
+export default ProfilePhoto;
